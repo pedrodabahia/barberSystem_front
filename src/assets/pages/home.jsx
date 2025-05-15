e316e537ffe2129cc3da80288af3c21bd2f52ec0
Pedro Matos <pedrohsmatosprog@gmail.com>
2025-05-15 17:44:24 -0300

    ...

--- src/assets/pages/home.jsx
+++ src/assets/pages/home.jsx
@@ -1,7 +1,11 @@
+import usuarioIcon from '../images/perfilIcon.png'
+
 function Home(){
     return(
         <>
-        <div class=""></div>
+        <div class="usuario">
+            <div className="buttonNav" id="usuarioIcon"><img src={usuarioIcon}/></div>
+        </div>
         </>
     )
 }
